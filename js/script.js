1d8d7ca946a9dd02ecd8d7974c6d7b0d1e1b6ef7
Filipe Santos <outerspaceguy@live.co.uk>
2022-09-08 16:57:05 -0300

    soraka's world

--- js/script.js
+++ js/script.js
@@ -3,17 +3,23 @@ inputTexto.focus()
 var inputPassword = document.querySelector("#input-password")
 var botão = document.getElementsByClassName("btn-login")
 
+var senha = "viva"
+
 function login() {
-    if(inputTexto.value.length == 0) {
-        alert("Please, type your username.")
-        inputTexto.focus()
-    } else if(inputPassword.value.length == 0) {
-        alert("Please, type your password.")
-        inputPassword.focus()
-    } else {
-        alert("Logging in to Soraka's World...")
-        inputTexto.value = ""
-        inputPassword.value = ""
-        inputTexto.focus()
-    }
+   if(inputTexto.value.length == 0) {
+    alert("Please, type your username.")
+    inputTexto.focus()
+   } else if(inputPassword.value.length == 0) {
+    alert("Please, type your password.")
+    inputPassword.focus()
+   } else if(inputPassword.value != senha) {
+    alert("Wrong password!")
+    inputPassword.value = ""
+    inputPassword.focus()
+  } else {
+    alert("Logging in to Soraka's World...")
+    inputTexto.value = ""
+    inputPassword.value = ""
+    inputTexto.focus()
+  }
 }
